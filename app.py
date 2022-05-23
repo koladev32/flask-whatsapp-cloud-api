4826efa515a9b4230f64e67bcb108378b605dc63
koladev <onaelmangabo@gmail.com>
2022-05-23 23:16:05 +0100

    ✨ add whatsapp api wrapper

--- app.py
+++ app.py
@@ -2,20 +2,19 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    return "Hello World!"
 
-@app.route('/send_message/', methods=['POST'])
+
+@app.route("/send_message/", methods=["POST"])
 def send_message():
-    """_summary_: Send a message with a template to a phone number
-    """
+    """_summary_: Send a message with a template to a phone number"""
     pass
 
-@app.route('/get_message/', methods=['GET'])
+
+@app.route("/get_message/", methods=["GET"])
 def get_message():
-    """ __summary__: Get message from the webhook
-    """
+    """__summary__: Get message from the webhook"""
     pass
-    
-    
--- whatsapp_client.py
+++ whatsapp_client.py
@@ -1,8 +1,53 @@
-import os 
+import os
 
 import requests
 
 
-class WhatsAppWrapper: 
+class WhatsAppWrapper:
+
+    API_URL = "https://graph.facebook.com/v13.0/"
+    API_TOKEN = os.environ.get("WHATSAPP_API_TOKEN")
+    NUMBER_ID = os.environ.get("WHATSAPP_NUMBER_ID")
+
+    def __init__(self):
+        self.headers = {
+            "Authorization": "Bearer {}".format(self.API_TOKEN),
+            "Content-Type": "application/json",
+        }
+
+        self.API_URL = "https://graph.facebook.com/v13.0/" + self.NUMBER_ID
+
+    def send_template_message(self, template_name, language_code, phone_number):
+
+        data = {
+            "messaging_product": "whatsapp",
+            "to": phone_number,
+            "type": "template",
+            "template": {"name": template_name, "language": {"code": language_code}},
+        }
+        
+        response = requests.post(f"{self.API_URL}/messages", headers=self.headers, json=data)
+        
+        assert response.status_code == 200, "Error sending message"
+        
+        return response.json()
     
-    API_KEY = None
+    def process_webhook_notification(self, data):
+        """_summary_: Process webhook notification
+            For the moment, this will return the type of notification
+        """
+        
+        response =  []
+        
+        for entry in data['entry']:
+            
+            for change in entry['changes']:
+                response.append({
+                    "type": change['field'],
+                    "from": change['display_phone_number'],
+                    "messages": change['messages'],
+                    "errors": change['errors'],
+                    "contacts": change['contacts'],
+                })
+                
+        return response
